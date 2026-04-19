1fc604d5bcd297723c7f1b6cb7eb5a7f89221f98
ajimen49 <ajimen49@xtec.cat>
2026-04-19 17:13:16 +0200

    Refactor config.js to include tagMap and remove cards
    Removed card definitions and added tag mapping.

---
+++ generator/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectionId: "laliga-2025",
+  outputFile: "../data/laliga-2025.js",
+
+  tagMap: [
+    { match: "momentum oro", tag: "momentum_oro" },
+    { match: "premium oro", tag: "premium_oro" },
+    { match: "premium", tag: "premium" },
+    { match: "pocket box", tag: "pocket_box" },
+    { match: "dream box", tag: "dream_box" },
+    { match: "tin box", tag: "tinbox" },
+    { match: "revista", tag: "revista" },
+    { match: "adrenalyn plus", tag: "adrenalyn_plus" }
+  ]
+};
